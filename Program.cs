ddf52b42a85dacff0a8670a28728b5f9a35790ad
imran zaman <imran_ujhani@yahoo.com>
2020-06-01 00:39:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] unorderedScores = new [] { 2, 6, 3,6, 1,6,2 };
+            int highestPossibleScore = 10;
+            TopScore.SortScores(unorderedScores,highestPossibleScore);
+            
+        }
+    }
+}
---
+++ TopScore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeConsoleApp
+{
+    public class TopScore
+    {
+        public static int[] SortScores(int[] unorderedScores, int highestPossibleScore)
+        {
+            // Array of 0s at indices 0..highestPossibleScore
+            int[] scoreCounts = new int[highestPossibleScore + 1];
+
+            // Populate scoreCounts
+            foreach (var score in unorderedScores)
+            {
+                scoreCounts[score]++;
+            }
+
+            // Populate the final sorted array
+            int[] sortedScores = new int[unorderedScores.Length];
+            int currentSortedIndex = 0;
+
+            // For each item in scoreCounts
+            for (int score = highestPossibleScore; score >= 0; score--)
+            {
+                int count = scoreCounts[score];
+
+                // For the number of times the item occurs
+                for (int occurrence = 0; occurrence < count; occurrence++)
+                {
+                    // Add it to the sorted array
+                    sortedScores[currentSortedIndex] = score;
+                    currentSortedIndex++;
+                }
+            }
+
+            return sortedScores;
+        }
+
+    }
+}

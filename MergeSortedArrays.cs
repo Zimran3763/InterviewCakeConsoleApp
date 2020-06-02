2c946f95339be163d33e92401f3ee664ca357b9e
imran zaman <imran_ujhani@yahoo.com>
2020-06-02 19:13:34 -0400

    MergeSortedArray

---
+++ MergeSortedArrays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCakeConsoleApp
+{
+    public class MergeSortedArrays
+    {
+        public static int[]  MergeArrays(int[] array1, int[] array2)
+        {
+            var result = new int [array1.Length + array2.Length] ;
+            int array1Index = 0;
+            int array2Index = 0;
+            int resultArrayIndex = 0;
+           
+
+            for(resultArrayIndex = 0; resultArrayIndex < result.Length; resultArrayIndex ++)
+            {
+                bool array1Exhausted = array1Index >= array1.Length;
+                bool array2Exhausted = array2Index >= array2.Length;
+                // Case: next elements in result array comes from my array1
+                // array1 must not be exhausted, and EITHER:
+                // 1) array2 IS exhausted, or
+                // 2) the current element in array1 is less
+                //    than the current element in array2
+                if ( !array1Exhausted && (array2Exhausted ||( array1[array1Index]<array2[array2Index])))
+                {
+                    result[resultArrayIndex] = array1[array1Index];
+                    array1Index++;
+                }
+                else
+                {
+                    // Case: next elements in result array comes from array2
+                    result[resultArrayIndex] = array2[array2Index];
+                    array2Index++;
+                }
+                
+            }
+            return result;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,13 +6,22 @@ using System.Threading.Tasks;
 
 namespace InterviewCakeConsoleApp
 {
-    class Program
+    public class Program
     {
+   
         public static void Main(string[] args)
         {
             int[] unorderedScores = new [] { 2, 6, 3,6, 1,6,2 };
             int highestPossibleScore = 10;
             TopScore.SortScores(unorderedScores,highestPossibleScore);
+
+            int[] array1 = { 3, 4, 6, 10, 11, 15 };
+            int[] array2 = { 1, 5, 8, 12, 14, 19 };
+            var arrayResult =  MergeSortedArrays.MergeArrays(array1, array2);
+            foreach (var i in arrayResult)
+            {
+                Console.Write("{0} ", i);
+            }
             
         }
     }
